40bd16a44103152c460dbdad23110f14e49cbff4
mikolajkozlowskiii <mikolajkozlowskiii@gmail.com>
2024-08-06 22:19:15 +0200

    Refactor

--- backend/src/main/java/com/javamaster/TicTacToeApplication.java
+++ backend/src/main/java/com/javamaster/TicTacToeApplication.java
@@ -3,9 +3,6 @@ package com.javamaster;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 @SpringBootApplication
 public class TicTacToeApplication {
 
